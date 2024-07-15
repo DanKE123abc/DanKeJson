d5a6f34ca76bad9c4c1c7042ba8f8f9f37cfee64
DanKe <1586017466@QQ.com>
2024-07-15 13:12:59 +0800

    创建 JSON5 类
    虽然JSON类有点挂羊头卖狗肉，实际上它也可以解析json5内的部分功能了，但为了之后序列化成json5与传统json分开，还是创建一个新类吧，但反序列化json5实际上JSON类和JSON5类都可以，序列化json5格式以及修改标注json5格式的功能只给JSON5类

---
+++ DanKeJson/JSON5.cs
@@ -0,0 +1,73 @@
+﻿#region Header
+
+/*
+* MIT License
+
+Copyright (c) 2023 - present DanKe
+
+* DanKeJson
+a simple Json library for the .Net
+JSON : https://json.org
+
+* About Author
+Name : DanKe
+Address : Guangzhou City , Guangdong Province , China
+Mail : danke1024@foxmail.com
+Github : https://github.com/DanKE123abc
+  */
+
+#endregion
+
+#pragma warning disable CS8604
+#pragma warning disable CS8603
+#pragma warning disable CS8602
+#pragma warning disable CS8600
+#pragma warning disable CS1591
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+namespace DanKeJson
+{
+
+    /// <summary>
+    /// DanKeJson : Serialization and Deserialization
+    /// </summary>
+    public static class JSON5
+    {
+        /// <summary>
+        /// Serializing Json5(String) to JsonData
+        /// About Json5 : https://json5.org
+        /// Using comments can affect performance
+        /// </summary>
+        /// <param name="text">the JsonText</param>
+        /// <param name="useComments">Using CommentParser.cs to Use Comments</param>
+        /// <param name="skipFileCheck">Skip the file path check</param>
+        /// <returns>JsonData</returns>
+        public static JsonData ToData(string text, bool useComments = false, bool skipFileCheck = false)
+        {
+            return JSON.ToData(text, useComments, skipFileCheck);
+        }
+
+        /// <summary>
+        /// Serializing Json5(String) to Class
+        /// About Json5 : https://json5.org
+        /// Using comments can affect performance
+        /// </summary>
+        /// <param name="text">the JsonText</param>
+        /// <param name="useComments">Using CommentParser.cs to Use Comments</param>
+        /// <param name="skipFileCheck">Skip the file path check</param>
+        /// <typeparam name="T">Class</typeparam>
+        /// <returns>T Class</returns>
+        public static T ToData<T>(string text, bool useComments = false, bool skipFileCheck = false) where T : class, new()
+        {
+            return JSON.ToData<T>(text, useComments, skipFileCheck);
+        }
+    }
+    
+}
